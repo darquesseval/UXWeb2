d7af897d9425ec463106f9cacbd6dc5ed0d75da9
darquesseval <nikita.szedalik@stud.hslu.ch>
2022-03-09 11:57:25 +0100

    url

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var server = app.listen(process.env.PORT || 7000, function(){
+var server = app.listen(process.env.PORT || 'https://experimenting-webux2.herokuapp.com', function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
