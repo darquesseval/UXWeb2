dada3f60378bd172c9e147e15d30a383bd057be3
darquesseval <nikita.szedalik@stud.hslu.ch>
2022-03-09 09:51:55 +0100

    smartphone first setup

---
+++ Exercises/Smartphone_Sensor/public/sketch.js
@@ -0,0 +1,33 @@
+var socket
+
+function setup() {
+createCanvas(windowWidth, windowHeight)
+socket = io.connect('https://kind-kowalevski-48d942.netlify.app/exercises/Smartphone_Sensor/public/')
+socket.on('mouse', newDrawing);
+background(0);
+}
+
+function newDrawing(data) {
+    noStroke()
+    fill(0,255,255);
+    ellipse(data.x,data.y,50)
+}
+
+function mouseDragged() {
+    console.log('Sending: ' + mouseX + ',' + mouseY);
+
+    var data = {
+        x: mouseX,
+        y: mouseY
+    }
+
+    socket.emit('mouse', data);
+
+    fill(255, 0, 255);
+    noStroke();
+    ellipse(mouseX,mouseY,50);
+}
+
+function draw() {
+
+}
