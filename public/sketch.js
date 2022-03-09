4ea3ad78d57f634c29627a8905807f36f872b3de
darquesseval <nikita.szedalik@stud.hslu.ch>
2022-03-09 13:05:46 +0100

    sketch.js back

--- public/sketch.js
+++ public/sketch.js
@@ -7,25 +7,27 @@ socket.on('mouse', newDrawing);
 background(0);
 }
 
+function newDrawing(data) {
+    noStroke()
+    fill(0,255,255);
+    ellipse(data.x,data.y,50)
+}
+
 function mouseDragged() {
-    socket.emit('mouse', data);
-
-    fill(255, 0, 255);
-    noStroke();
-    ellipse(mouseX,mouseY,50);
-
     console.log('Sending: ' + mouseX + ',' + mouseY);
 
     var data = {
         x: mouseX,
         y: mouseY
     }
+
+    socket.emit('mouse', data);
+
+    fill(255, 0, 255);
+    noStroke();
+    ellipse(mouseX,mouseY,50);
 }
-function newDrawing(data) {
-    noStroke()
-    fill(0,255,255);
-    ellipse(data.x,data.y,50)
-}
+
 function draw() {
 
 }
