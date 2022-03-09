321850e6e417aac6d24fbc4e86a4d967bd230eb1
darquesseval <nikita.szedalik@stud.hslu.ch>
2022-03-09 11:50:10 +0100

    app.listen

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-var server = app.listen('7000');
+var server = app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 app.use(express.static('public'));
 
