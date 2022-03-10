f50882bd4c7a3c4a08835f8f1adb90d8fc88683f
darquesseval <nikita.szedalik@stud.hslu.ch>
2022-03-10 12:44:59 +0100

    ?

--- public/sketch.js
+++ public/sketch.js
@@ -3,33 +3,72 @@ let engine
 let world
 
 function setup() {
-createCanvas(windowWidth, windowHeight)
-socket = io.connect('https://experimenting-webux2.herokuapp.com')
-socket.on('mouse', newDrawing);
-socket.on('angle', newDot);
-background(0);
-engine = Matter.Engine.create();
-world = engine.world;
+    createCanvas(windowWidth, windowHeight)
+    socket = io.connect('https://experimenting-webux2.herokuapp.com')
+    socket.on('mouse', newDrawing);
+    socket.on('angle', newDot);
+    background(0);
+    engine = Matter.Engine.create();
+    world = engine.world;
+}
+
+
+function draw() {
+    Matter.Engine.update(engine);
+    newDot();
+}
+
+// code from kickprog project
+class Circle {
+    constructor(x, y, r) {
+        this.body = Matter.Bodies.circle(x, y, r, {
+            //make physics a bit more sand-like
+            restitution: 0.35,
+            density: 1,
+            friction: 0.5,
+        });
+        Matter.World.add(world, this.body);
+        this.r = r;
+    }
+
+    //draw the body
+    show(colorR, colorG, colorB, colorO) {
+        const pos = this.body.position;
+        const angle = this.body.angle;
+        noStroke();
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        fill(colorR, colorG, colorB, colorO)
+        noStroke();
+        ellipse(0, 0, this.r * 2)
+        pop();
+    }
+
 }
 
 function newDot(dataSmartphone) {
     push()
-translate(windowWidth/2, windowHeight/2)
+    translate(windowWidth / 2, windowHeight / 2)
 
-// fill(255,0,0);
-// ellipse(dataSmartphone.angle1, dataSmartphone.angle2, 10);
+    // fill(255,0,0);
+    // ellipse(dataSmartphone.angle1, dataSmartphone.angle2, 10);
 
-let pointer = new Circle(dataSmartphone.angle1, dataSmartphone.angle2, 20);
-pointer.show(255,0,0,100)
-pop()
+    let pointer = new Circle(dataSmartphone.angle1, dataSmartphone.angle2, 20);
+    pointer.show(255, 0, 0, 100)
+    pop()
 
     console.log('received: ' + dataSmartphone)
 }
 
+
+
+
+
 function newDrawing(data) {
     noStroke()
-    fill(0,255,255);
-    ellipse(data.x,data.y,50)
+    fill(0, 255, 255);
+    ellipse(data.x, data.y, 50)
 }
 
 function mouseDragged() {
@@ -44,39 +83,5 @@ function mouseDragged() {
 
     fill(255, 0, 255);
     noStroke();
-    ellipse(mouseX,mouseY,50);
+    ellipse(mouseX, mouseY, 50);
 }
-
-function draw() {
-    Matter.Engine.update(engine);
-
-}
-
-// code from kickprog project
-class Circle {
-    constructor(x, y, r) {
-      this.body = Matter.Bodies.circle(x, y, r, {
-        //make physics a bit more sand-like
-        restitution: 0.35,
-        density: 1,
-        friction: 0.5,
-      });
-      Matter.World.add(world, this.body);
-      this.r = r;
-    }
-  
-    //draw the body
-    show(colorR, colorG, colorB, colorO) {
-      const pos = this.body.position;
-      const angle = this.body.angle;
-      noStroke();
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      fill(colorR, colorG, colorB, colorO)
-      noStroke();
-      ellipse(0, 0, this.r * 2)
-      pop();
-    }
-  
-  }
