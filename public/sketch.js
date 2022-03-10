31943b1927ce41a065e435257c74d503c20f0c72
darquesseval <nikita.szedalik@stud.hslu.ch>
2022-03-10 14:47:15 +0100

    constraint

--- public/sketch.js
+++ public/sketch.js
@@ -3,6 +3,9 @@ let engine
 let world
 let bottomB, topB, leftB, rightB
 
+let connection
+
+
 let pointer = []
 let i = 0
 
@@ -17,39 +20,54 @@ function setup() {
     engine = Matter.Engine.create();
     world = engine.world;
     rectMode(CENTER);
-bottomB = new Bound(0,windowHeight/2,windowWidth, 10);
-topB = new Bound(windowWidth/2,0,windowWidth,10);
-leftB = new Bound(0,windowHeight/2,10,windowHeight,1.5708);
-rightB = new Bound(windowWidth,windowHeight/2,10,windowHeight,1.5708);
+    bottomB = new Bound(0, windowHeight / 2, windowWidth, 10, 0);
+    topB = new Bound(windowWidth / 2, 0, windowWidth, 10, 0);
+    leftB = new Bound(0, windowHeight / 2, 10, windowHeight, 1.5708);
+    rightB = new Bound(windowWidth, windowHeight / 2, 10, windowHeight, 1.5708);
 }
 
 
 function draw() {
     Matter.Engine.update(engine);
-    bottomB.show(0,100,200,255);
-    topB.show(0,100,200,255);
-    leftB.show(0,100,200,255);
-    rightB.show(0,100,200,255);
-    if(pointer.length >=1) {
-    for(let n = 0; n<pointer.length;n++){
-        pointer[n].show(255, 0, 0, 255)
-    }
+    bottomB.show(0, 100, 200, 255);
+    topB.show(0, 100, 200, 255);
+    leftB.show(0, 100, 200, 255);
+    rightB.show(0, 100, 200, 255);
+   
+   push()
+   translate(windowWidth / 2, windowHeight / 2)
+    if(x) {
+let p1 = new Circle(windowWidth/2, windowHeight/5*3, 10)
+let p2 = new Circle(x, y, 10)
+p1.show(255);
+connection = Matter.Constraint.create({
+    bodyA: p1.body,
+    bodyB: Path2D.body,
+    length: windowHeight/5,
+    stiffness: 0.2
+})
+pop()
+
 }
+
+    // if (pointer.length >= 1) {
+    //     for (let n = 0; n < pointer.length; n++) {
+    //         pointer[n].show(255, 0, 0, 255)
+    //     }
+    // }
 }
 
 function newDot(dataSmartphone) {
-    push()
-    translate(windowWidth / 2, windowHeight / 2)
+    let x = dataSmartphone.angle1;
+    let y = dataSmartphone.angle2;
 
     // fill(255,0,0);
     // ellipse(dataSmartphone.angle1, dataSmartphone.angle2, 10);
 
-    pointer[i] = new Circle(dataSmartphone.angle1, dataSmartphone.angle2, 20);
-    
-    pop()
-    i++
+    // pointer[i] = new Circle(dataSmartphone.angle1, dataSmartphone.angle2, 20);
+    // i++
+
     console.log('received: ' + dataSmartphone)
-    console.log(i)
 }
 
 // code from kickprog project
@@ -83,30 +101,30 @@ class Circle {
 
 class Bound {
     constructor(x, y, w, h, rotation) {
-      this.body = Matter.Bodies.rectangle(x, y, w, h, {
-        //these bodies do not move
-        isStatic: true,
-      });
-      Matter.Body.rotate(this.body, rotation);
-      Matter.World.add(world, this.body);
-      this.w = w;
-      this.h = h;
+        this.body = Matter.Bodies.rectangle(x, y, w, h, {
+            //these bodies do not move
+            isStatic: true,
+        });
+        Matter.Body.rotate(this.body, rotation);
+        Matter.World.add(world, this.body);
+        this.w = w;
+        this.h = h;
     }
-  
+
     //draw the body
     show(colorR, colorG, colorB, colorO) {
-      rectMode(CENTER);
-      const pos = this.body.position;
-      const angle = this.body.angle;
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      fill(colorR, colorG, colorB, colorO);
-      strokeWeight(3);
-      rect(0, 0, this.w, this.h, 5);
-      pop();
+        rectMode(CENTER);
+        const pos = this.body.position;
+        const angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        fill(colorR, colorG, colorB, colorO);
+        strokeWeight(3);
+        rect(0, 0, this.w, this.h, 5);
+        pop();
     }
-  }
+}
 
 
 function newDrawing(data) {
