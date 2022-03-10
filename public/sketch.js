185ac05dbb42b35db53df43e5a23e26c584a641c
darquesseval <nikita.szedalik@stud.hslu.ch>
2022-03-10 13:07:58 +0100

    lala

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,11 @@
 var socket
 let engine
 let world
+let bottomB, topB, leftB, rightB
+
 let pointer = []
 let i = 0
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
@@ -12,11 +15,25 @@ function setup() {
     background(0);
     engine = Matter.Engine.create();
     world = engine.world;
+    angleMode(DEGREES);
+bottomB = new Bound(0,windowHeight,windowWidth, 10);
+topB = new Bound(0,0,windowWidth,10);
+leftB = new Bound(0,0,10,windowHeight,90);
+rightB = new Bound(windowWidth,0,10,windowHeight,90);
 }
 
 
 function draw() {
     Matter.Engine.update(engine);
+    bottomB.show(0,100,200,255);
+    topB.show(0,100,200,255);
+    leftB.show(0,100,200,255);
+    rightB.show(0,100,200,255);
+    if(pointer.length <=1) {
+    for(let n = 0; n<pointer.length;n++){
+        pointer[n].show(255, 0, 0, 255)
+    }
+}
 }
 
 function newDot(dataSmartphone) {
@@ -27,10 +44,11 @@ function newDot(dataSmartphone) {
     // ellipse(dataSmartphone.angle1, dataSmartphone.angle2, 10);
 
     pointer[i] = new Circle(dataSmartphone.angle1, dataSmartphone.angle2, 20);
-    pointer[i].show(255, 0, 0, 100)
+    
     pop()
     i++
     console.log('received: ' + dataSmartphone)
+    console.log(i)
 }
 
 // code from kickprog project
@@ -61,6 +79,33 @@ class Circle {
     }
 
 }
+
+class Bound {
+    constructor(x, y, w, h, rotation) {
+      this.body = Matter.Bodies.rectangle(x, y, w, h, {
+        //these bodies do not move
+        isStatic: true,
+      });
+      Matter.Body.rotate(this.body, rotation);
+      Matter.World.add(world, this.body);
+      this.w = w;
+      this.h = h;
+    }
+  
+    //draw the body
+    show(colorR, colorG, colorB, colorO) {
+      rectMode(CENTER);
+      const pos = this.body.position;
+      const angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      fill(colorR, colorG, colorB, colorO);
+      strokeWeight(3);
+      rect(0, 0, this.w, this.h, 5);
+      pop();
+    }
+  }
 
 
 function newDrawing(data) {
