2eba76343f692625e0a4a6c2ba8386cccc44d6e5
darquesseval <nikita.szedalik@stud.hslu.ch>
2022-03-18 11:05:19 +0100

    pls

--- public/sketch.js
+++ public/sketch.js
@@ -9,7 +9,7 @@ let circleChain
 let gap;
 let posX;
 let posY;
-let memberY
+let armY, tentacleY;
 
 let bg, bg_stunned, bell, tentacle, arm, mouth;
 
@@ -17,6 +17,7 @@ let fix1, fix2, fix3, fix4, fix5, fix6, fix7, fix8, fix9, fix10, fix11;
 let arm1, tentacle1, arm2, tentacle2, arm3, tentacle3, arm4, tentacle4, arm5, tentacle5, tentacle6;
 let arm1x, tentacle1x, arm2x, tentacle2x, arm3x, tentacle3x, arm4x, tentacle4x, arm5x, tentacle5x, tentacle6x;
 let bellPhysics, bellH, bellW;
+let armGap, tentacleGap;
 
 function preload() {
     bg = loadImage('https://rocky-fjord-59052.herokuapp.com/https://kind-kowalevski-48d942.netlify.app/public/pic/background.jpeg');
@@ -40,10 +41,13 @@ function setup() {
     world = engine.world;
     posX = windowWidth / 2;
     posY = windowWidth*0.3;
-    memberY = posY-windowWidth/90
+    tentacleY = posY-windowWidth/50;
+    armY = posY-windowWidth/20;
     bellW = windowWidth/13*2;
     bellH = windowWidth/10;
     gap = windowWidth/10
+    armGap = windowWidth / gap;
+    tentacleGap = windowWidth / gap/8;
     
     arm1x = -windowWidth/40*2
     arm2x = -windowWidth/40*1
@@ -72,20 +76,20 @@ function setup() {
     rectMode(CENTER);
     // angleMode(DEGREES);
     world.gravity.y = 0;
-    arm1 = new Rope(posX+arm1x, memberY, windowWidth / gap, 8, fix1)
-    arm2 = new Rope(posX+arm2x, memberY, windowWidth / gap, 8, fix2)
-    arm3 = new Rope(posX+arm3x, memberY, windowWidth / gap, 8, fix3)
-    arm4 = new Rope(posX+arm4x, memberY, windowWidth / gap, 8, fix4)
-    arm5 = new Rope(posX+arm5x, memberY, windowWidth / gap, 8, fix5)
+    arm1 = new Rope(posX+arm1x, armY, armGap, 8, fix1)
+    arm2 = new Rope(posX+arm2x, armY, armGap, 8, fix2)
+    arm3 = new Rope(posX+arm3x, armY, armGap, 8, fix3)
+    arm4 = new Rope(posX+arm4x, armY, armGap, 8, fix4)
+    arm5 = new Rope(posX+arm5x, armY, armGap, 8, fix5)
 
-    tentacle1 = new Rope(posX+tentacle1x, memberY, windowWidth / gap/8, 60, fix6)
-    tentacle2 = new Rope(posX+tentacle2x, memberY, windowWidth / gap/8, 60, fix7)
-    tentacle3 = new Rope(posX+tentacle3x, memberY, windowWidth / gap/8, 60, fix8)
-    tentacle4 = new Rope(posX+tentacle4x, memberY, windowWidth / gap/8, 60, fix9)
-    tentacle5 = new Rope(posX+tentacle5x, memberY, windowWidth / gap/8, 60, fix10)
-    tentacle6 = new Rope(posX+tentacle6x, memberY, windowWidth / gap/8, 60, fix11)
+    tentacle1 = new Rope(posX+tentacle1x, tentacleY, tentacleGap, 60, fix6)
+    tentacle2 = new Rope(posX+tentacle2x, tentacleY, tentacleGap, 60, fix7)
+    tentacle3 = new Rope(posX+tentacle3x, tentacleY, tentacleGap, 60, fix8)
+    tentacle4 = new Rope(posX+tentacle4x, tentacleY, tentacleGap, 60, fix9)
+    tentacle5 = new Rope(posX+tentacle5x, tentacleY, tentacleGap, 60, fix10)
+    tentacle6 = new Rope(posX+tentacle6x, tentacleY, tentacleGap, 60, fix11)
 
-    bellPhysics = Matter.Bodies.trapezoid(posX+windowWidth/120, posY-bellH/2, bellW, bellH, 1, {
+    bellPhysics = Matter.Bodies.trapezoid(posX+windowWidth/90, posY-bellH/2, bellW, bellH, 1, {
         isStatic: true
     })
 
@@ -175,7 +179,7 @@ function tentaclesTurn(dataSmartphone) {
     translate(bellPhysics.position.x,bellPhysics.position.y+bellH/2)
         let rotationAngle = Math.atan2(dataSmartphone.angle1, dataSmartphone.angle2)*(180 / Math.PI);      
         angleMode(DEGREES);
-        rotate(rotationAngle)
+        bellPhysics.angle = rotationAngle;
         pop();
 }
 
