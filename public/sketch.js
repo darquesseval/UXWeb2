909fd7172a7c8e85b49cd9c84ebcb528086e985c
darquesseval <nikita.szedalik@stud.hslu.ch>
2022-03-20 14:46:47 +0100

    .

--- public/sketch.js
+++ public/sketch.js
@@ -48,7 +48,7 @@ function setup() {
     armY = posY-windowWidth/50;
     bellW = windowWidth/13*2;
     bellH = windowWidth/10;
-    gap = windowWidth/9
+    gap = windowWidth/7
     armGap = windowWidth / gap;
     tentacleGap = windowWidth / gap/8;
     
